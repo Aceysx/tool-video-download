80f894d4ed821983dc40ecaf56c4f4925e8befb5
sixin <xinsi@thoughtworks.com>
2025-10-10 19:44:10 +0800

    feat: refine

--- src/components/analytics/google-analytics.tsx
+++ src/components/analytics/google-analytics.tsx
@@ -5,10 +5,7 @@ const GA_MEASUREMENT_ID = 'G-2K4QQ6SDRY';
 export function GoogleAnalytics() {
     return (
         <>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-                strategy='lazyOnload'
-            />
+            <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} strategy='lazyOnload' />
             <Script id='google-analytics' strategy='lazyOnload'>
                 {`
                     window.dataLayer = window.dataLayer || [];
